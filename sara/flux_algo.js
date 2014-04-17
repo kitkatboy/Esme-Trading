2dedc06d6bc0f88f557bd9f2c806b1a17c16bc74
Sara Atek <sara_atek@hotmail.fr>
2014-04-17 20:42:55 +0200

    verssion 17/04/2014

--- sara/flux_algo.js
+++ sara/flux_algo.js
@@ -7,35 +7,29 @@ var savePoids = new EventEmitter();
 var server ={};
 server.nbr=0;
 server.i=0;
-server.path = "../";
-
-event.on("algo", function(){
-while(server.nbr<1) // TODO 39
-{
-  
-server.calcul(server.nbr++); // appel a la fonction qui exctrai les données de l'entreprise i
-
-}
-});
+server.path = "./";
+server.cac_40={"entreprise": []};
 
 server.calcul= function(num_entreprise)
-{   
+{ 
 	fs.exists(server.path+'entreprises_cac40.js', function(exist){						
 	if(exist)	// si le fichier n'existe pas on passe a l'entreprise suivante 					
-		{
+		{  
 			fs.readFile(server.path+'entreprises_cac40.js', 'utf-8', 'r+', function (err, d) {
 			if (err) throw err;
 			d = JSON.parse(d);
 			var nom= d.nom[num_entreprise].nom;
+			console.log(nom);
 			fs.exists(server.path+''+nom, function(exist){
 				if(exist) // on ouvre le fichier que si il existe
 				{
 					fs.readFile(server.path+''+nom, 'utf-8', 'r+', function (err, data) {
 						if (err) throw err;
-						data = JSON.parse(data);
-						if(data.jour)
+						data = JSON.parse(data);					
+						if(data)
 						{
 							server.calcul_droites(data, num_entreprise);
+							
 						}
 					});
 				}
@@ -46,29 +40,25 @@ server.calcul= function(num_entreprise)
 		
 		}
 	});	
+
 };	
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 server.calcul_droites=function(data, num_entreprise){
-if(data.jour.length>5) // SI ON A UNE SEMAINE DE DONNEES
-{
 	var nbr_valeure_semaine = 5 * data.jour[data.jour.length-2].heure.length; 
 	var derniere_valeure_semaine =data.jour[data.jour.length-2].heure[data.jour[data.jour.length-2].heure.length-1].valeure;
-	derniere_valeure_semaine=derniere_valeure_semaine.split(',');
-	derniere_valeure_semaine=derniere_valeure_semaine[0]+"."+derniere_valeure_semaine[1]
+	derniere_valeure_semaine=derniere_valeure_semaine.replace(",", ".");           
 	var premiere_valeure_semaine=data.jour[data.jour.length-6].heure[0].valeure
-	premiere_valeure_semaine=premiere_valeure_semaine.split(',');
-	premiere_valeure_semaine=premiere_valeure_semaine[0]+"."+premiere_valeure_semaine[1]
-	var coef_dirrecteur_semaine = Number(nbr_valeure_semaine) / (Number(derniere_valeure_semaine) - Number(premiere_valeure_semaine));
-
+	premiere_valeure_semaine=premiere_valeure_semaine.replace(",", ".");
+	var coef_dirrecteur_semaine = 0;
+	coef_dirrecteur_semaine = ((+derniere_valeure_semaine)- (+premiere_valeure_semaine) )/(+nbr_valeure_semaine) ;//
 	var nbr_valeure_jour = data.jour[data.jour.length-2].heure.length; 
 	var derniere_valeure_jour =data.jour[data.jour.length-1].heure[data.jour[data.jour.length-1].heure.length-1].valeure;
-	derniere_valeure_jour=derniere_valeure_jour.split(',');
-	derniere_valeure_jour=derniere_valeure_jour[0]+"."+derniere_valeure_jour[1]
+	derniere_valeure_jour=derniere_valeure_jour.replace(",", ".");
 	var premiere_valeure_jour=data.jour[data.jour.length-1].heure[0].valeure
-	premiere_valeure_jour=premiere_valeure_jour.split(',');
-	premiere_valeure_jour=premiere_valeure_jour[0]+"."+premiere_valeure_jour[1]
-	var coef_dirrecteur_jour = Number(nbr_valeure_jour) / (Number(derniere_valeure_jour) - Number(premiere_valeure_jour));
-
+	premiere_valeure_jour=premiere_valeure_jour.replace(",", ".");
+	var coef_dirrecteur_jour =0;
+	coef_dirrecteur_jour = (+derniere_valeure_jour -(+premiere_valeure_jour)) /(+nbr_valeure_jour) ; //
+	
 	var droite_semaine ={};
 	var droite_jour={};
 
@@ -85,7 +75,7 @@ if(data.jour.length>5) // SI ON A UNE SEMAINE DE DONNEES
 		{
 			event.emit("vecteur", data, droite_semaine, droite_jour, num_entreprise);
 		}
-}
+
 };	
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 event.on("vecteur", function(data, droite_semaine, droite_jour, num_entreprise){
@@ -95,12 +85,12 @@ var vecteur_jour={"valeure" :[]};
 
 	for(val=0; val<droite_semaine.nbr_valeure_semaine; val++)
 	{
-		vecteur_semaine.valeure[val] = Number(droite_semaine.coef_dirrecteur) * (val) + Number(droite_semaine.valeure_origine);	
+		vecteur_semaine.valeure[val] = +(droite_semaine.coef_dirrecteur) * (val) + Number(droite_semaine.valeure_origine);	
+			
 	}
-
 	for(tps=0; tps<droite_jour.nbr_valeure_jour; tps++)
 	{
-		vecteur_jour.valeure[tps]= Number(droite_jour.coef_dirrecteur) * (5*tps) + Number(droite_jour.valeure_origine);
+		vecteur_jour.valeure[tps]= +(droite_jour.coef_dirrecteur) * (5*tps) + Number(droite_jour.valeure_origine);
 	}
 		
 	if(vecteur_semaine.valeure && vecteur_jour.valeure)
@@ -111,9 +101,8 @@ var vecteur_jour={"valeure" :[]};
 });
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 event.on("compare", function(data, vecteur_semaine, vecteur_jour, num_entreprise){
-var cac_40={"entreprise": []};
-var nbr_jours_semaine= data.jour.length-1;
-var nbr_valeur_jour= data.jour[data.jour.length-1].heure.length;
+var nbr_jours_semaine= data.jour.length-2;
+var nbr_valeur_jour= data.jour[data.jour.length-2].heure.length;// todo peu poser probleme si le nombre de val n'est pas constant
 var val_courrante_semaine;
 var val_courrante_jour=0;
 var variance_semaine = 0;
@@ -123,10 +112,8 @@ var poids_jour=0;
 var val=0;
 var tmp=0;
 var json={};
-cac_40.nom_entreprise=data.nom_entreprise;
-//console.log(vecteur_jour);
-//console.log(vecteur_semaine);
-//console.log(require("util").inspect(vecteur_semaine));
+
+
 for(i=0; i< nbr_jours_semaine; i++)
 	{
 		for(j=0; j< nbr_valeur_jour; j++)
@@ -136,6 +123,7 @@ for(i=0; i< nbr_jours_semaine; i++)
 				val_courante=data.jour[i].heure[j].valeure;
 				val_courante = val_courante.replace(",", ".");
 				variance_semaine += Math.pow((+val_courante - vecteur_semaine.valeure[val]), 2);
+				
 				val++;
 				if(data.jour[i].heure[j+1] && data.jour[i].heure[j+1] != undefined)
 					{
@@ -152,8 +140,7 @@ for(i=0; i< nbr_jours_semaine; i++)
 		
 	}
 	variance_semaine=(+1/nbr_jours_semaine) * variance_semaine;
-	//console.log(poids_semaine);
-	//console.log(variance_semaine);
+
 	for(i=0; i<data.jour[data.jour.length-1].heure.length; i++) // parcours des valeurs du jour courrant 
 	{ 
 		if(data.jour[data.jour.length-1].heure[i])
@@ -174,27 +161,30 @@ for(i=0; i< nbr_jours_semaine; i++)
 	
 	}
 	variance_jour = (+1/nbr_valeur_jour) * variance_jour;
-	console.log(poids_jour);
-	console.log(variance_jour);
+	
+	
+	
 	// remplissage du json 
 	json.poids_semaine=poids_semaine;
 	json.variance_semaine=variance_semaine;
 	json.poids_jour=poids_jour;
 	json.variance_jour=variance_jour;
-	cac_40.entreprise[num_entreprise] = json;
-	if(cac_40.entreprise)
+	json.nom_entreprise=data.nom_entreprise;
+	server.cac_40.entreprise[num_entreprise] = json;
+	
+	if(server.cac_40.entreprise)
 	{
-		event.emit("enregistrement", cac_40);
+		event.emit("enregistrement", server.cac_40);
 	}
 	
 });
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////			
 
 event.on("enregistrement", function(cac_40){
-	fs.writeFile("cac_40", JSON.stringify(cac_40), 'utf8', 'a+', function(err){
+	fs.writeFile("cac_40.js", JSON.stringify(cac_40), 'utf8', 'a+', function(err){
 		if(err) throw err;
 		console.log('we just saved the buffer in a file');
-		//event.emit("algo");// DECLANCHEMENT D4UN EVENEMENT POUR PASS2 A L4ENTREPRISE SUIVANTE //TODO
+		server.calcul(server.nbr++);
 		}); 
 					
 	}); 
@@ -203,7 +193,8 @@ event.on("enregistrement", function(cac_40){
 
 var start=function()
 {
-	event.emit("algo");
-}
+	server.calcul(server.nbr++);
 
+}
+//start();
 exports.traitement= start;
--- sara/flux_cac40.js
+++ sara/flux_cac40.js
@@ -1,5 +1,6 @@
 var util = require('util');
 var http = require('http');
+var net = require('net');
 var demarrage = require('./flux_json.js');
 var fin=require('./flux_algo.js');
 var fs = require('fs');
@@ -14,21 +15,15 @@ server.Annee=server.Today.getFullYear();
 server.i=0; // compteur d'entreprise 
 server.jour=0;
 server.heure=-1;
-server.requettes=-1; // nombre de requette pour recuperer les données courantes 
  if(server.Mois.length == 1) {
         server.Mois = '0' + server.Mois;
 		 
     }
-server.path = "../";
-server.compteur=0; // mon semaphore
+server.path = "./";
+server.compteur=0; // semaphore
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
-	
-//////////////////////////////////////////////////////FONCTION QUI SE DECLANCHE APRES L4EVENEMNT DE DEMANDE DE RAFRAICHISSEMENT DES DONN2ES DOURSIERE COT2 CLIENT//////////////////////////////////////////////////////////////////////
-emetteur.on('rafraichissement', function(){ 
-	server.extractionDonees(0); // appel a la fonction qui exctrai les données de l'entreprise i
-	//server.requettes+=1; // on incremente le nombres de requette effectuée corresponds au nombres de 5 min
-	server.i=0; // on remet le COMPTEUR d'entreprise a 0 a chaque nouvelle requette 
-});
+var socket =new net.Socket();
+socket.setKeepAlive(true);
 
 ////////////////////////////////////////////////////////fonction qui recupere les flux html des données journaliere DE L4ENTREPRISE PASS2 EN ARGUMENT///////////////////////////////////
 server.extractionDonees=function(i){
@@ -38,39 +33,47 @@ var nom="";
 		fs.readFile(server.path+'entreprises_cac40.js', 'utf-8', 'r+', function (err, data) {
 		if (err) throw err;
 		data = JSON.parse(data);
-		symboles = data.nom[i].symbole;
+		var symboles = data.nom[i].symbole;
 		var nom = data.nom[i].nom//ADD
 		console.log(i);
 		console.log(symboles);
 				
         server.api = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Ffr.finance.yahoo.com%2Fq%2Fhp%3Fs%3D"+symboles+"%26b%3D10%26a%3D02%26c%3D2014%26e%3D"+server.Jours+"%26d%3D"+server.Mois+"%26f%3D"+server.Annee+"%26g%3Dd%22&format=json&diagnostics=true&callback=";
-		if(server.api);
-			{   
-				emetteur.emit("getBuffer", nom);//ADD
-				server.compteur++; // TODO 
-				
-			}		
+		  
+		emetteur.emit("getBuffer", nom);
+		server.compteur++; // semaphore
+								
 		});
 	
 }; ////////////////////////////////////////////////////FIN FONCTION RECUPERATION DONN2ES JOURNALIERE//////////////////////////////////////////
 	
 //////////////////////////////////////////////////////fonction qui recupere le resultat de la requette dans le buffeur et le met dans un json /////////////////////////////////////////	
 emetteur.on("getBuffer", function(nom) {
-		var b = ""; // variable qui va contenir les données resultante de la requette
-		http.get(server.api, function (r) {
-		console.log(r.statusCode+"ssssssssssssssssssss");
+//	socket.remotePort;//ADD
+//console.log(socket.localPort+"portttttttt");//ADD
+//	socket.setKeepAlive(true);//ADD
+	var b = ""; // variable qui va contenir les données resultante de la requette
+	http.get(server.api, function (r) {
+	console.log("statue code :"+r.statusCode);
 	if(r.statusCode==200)
 	{
         r.on("data", function (d) {
-            b += d;
-			
+            b += d;	
         });
         r.on("end", function() {
         b = JSON.parse(b);
 		emetteur.emit("json", b, nom); // une fois l'extration finie on declanche l'evenement qui enregistre les données utiles dans un json 	
 		});
+		r.on("socket", function (socket) { //ADD
+		socket.emit("agentRemove");//ADD
+		console.log("erreur socket");
+		});//ADD
+		r.on("error", function (e) { //ADD
+		console.log("une erreure c produite + e");//ADD
+		});//ADD
 	}else {	
 	server.compteur--;
+	console.log("une erreur");
 	emetteur.emit('recuperation');	
 	}
 		});
@@ -86,47 +89,47 @@ emetteur.on("json", function(b, nom){
 	if(b.query.results)
 	{
 		if(b.query.results.body.div[3].div[3].div[0].div.div.div.div[1].div.span[2].span.span)// car souvants des erreurs
-			{	console.log(server.jour+"jouuuuuuur");
-				console.log(server.heure+"heure");
+			{	console.log("jour :"+server.jour);
+				console.log("enregistrement numero :"+server.heure);
 				nom = b.query.results.body.div[3].div[3].div[0].div.div.div.div[0].div.h2;
-				//action_tmps_reel.time = b.query.results.body.div[3].div[3].div[0].div.div.div.div[1].div.span[2].span.span.content; // derniere heure a laquelle la donnée a été mise a jour
-				action_tmps_reel.time=new Date;
+				action_tmps_reel.time=new Date();
 				action_tmps_reel.valeure = b.query.results.body.div[3].div[3].div[0].div.div.div.div[1].div.span[0].span.content; //  donnée courante 
 				actions.heure[server.heure]=action_tmps_reel;
-				//actions.heure=action_tmps_reel;
-				actions.date= server.Today;
-				console.log(actions);
+		//actions.date= new Date();//ADD
 				action_entreprise.jour[server.jour]= actions;
 				action_entreprise.nom_entreprise=nom;
-				//action_entreprise.jour= actions;
-				console.log(action_entreprise);
+							
 					if(action_entreprise.jour[server.jour].heure[server.heure]) // si la recuperation de donnnée est fini ( non équale a nulll)
 						{
-						
 							emetteur.emit("enregistrement", action_entreprise, nom);
-						}
-					else{
+						}else{
 						emetteur.emit('recuperation');		
 						server.compteur--;
 					}
-			}
-		else {
+					
+			}else {
 			emetteur.emit('recuperation');		
 			server.compteur--;
 		}
-	}	
-	else {
+		
+	}else {
 		emetteur.emit('recuperation');		
 		server.compteur--;
-	}
-
+	}	
+	
+			
 });
 	
 //////////////////////////////////////////////FONCTION QUI ENREGISTRE LES DONN2ES dans un fichier json ////////////////////////////////////////////////////////
 emetteur.on("enregistrement", function(action_entreprise, nom){
-												
-fs.exists(nom, function(exist){						
-										
+fs.existsSync('/tmpFichier', function(ex){
+if(ex)						
+	{						
+	fs.unlinkSync('/tmpFichier') // on supprime le fichier temporaire 
+	console.log('successfull delete OF /tmpFile');
+	}
+});
+	fs.exists(nom, function(exist){															
 	if(exist)	// si le fichier n'existe pas on passe a l'entreprise suivante 					
 		{						
 			fs.readFile(nom, 'utf-8', 'r+', function (err, data) { // LE FICHIER PEUT EXISTER ET ETRE VIDE ! 
@@ -139,7 +142,6 @@ fs.exists(nom, function(exist){
 					if(err) throw err;
 					console.log(nom);
 					console.log('on a enregiste le buffer dans un fichier');
-					//server.compteur--;// semaphore
 					emetteur.emit('recuperation');// DECLANCHEMENT D4UN EVENEMENT POUR PASS2 A L4ENTREPRISE SUIVANTE
 					}); 
 				}else{
@@ -148,7 +150,6 @@ fs.exists(nom, function(exist){
 					if(err) throw err;
 					console.log(nom);
 					console.log('on a enregiste le buffer dans un fichier');
-					//server.compteur--; //semaphore
 					emetteur.emit('recuperation');// DECLANCHEMENT D4UN EVENEMENT POUR PASS2 A L4ENTREPRISE SUIVANTE
 					});	
 				}	
@@ -159,7 +160,6 @@ fs.exists(nom, function(exist){
 			if(err) throw err;
 			console.log(nom+" furst time");
 			console.log('on a enregiste le buffer dans un fichier');
-			//server.compteur --; // semaphore
 			emetteur.emit('recuperation');
 			}); 
 		}
@@ -167,27 +167,34 @@ fs.exists(nom, function(exist){
 		
 });	if(!--server.compteur)
 {
-	console.log("---------------------c'est fini");
-	//fin.traitement();
+		fs.writeFile("tmpFichier", "hello", function(err){ 
+		if(err) throw err;
+		console.log('on a creer le fichier temporaire');
+		});
+	console.log("---------------------c'est fini------------------------");
+	if(server.jour > 4)
+	{
+		fin.traitement();
+	}
 }
 });	
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 emetteur.on('recuperation', function(){
 	while(server.i<40)
-	{   
+	{   	
 		server.extractionDonees((server.i)++); // appel a la fonction qui exctrai les données de l'entreprise i +1
 	}
 
 });
 
-//------------------------------------------
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 emetteur.on('debut',function(){
 server.Today= new Date;
-if(server.Today.getHours()>8 && server.Today.getHours()<18 && server.Today.getDay()!=6 && server.Today.getDay()!=0) // heure de démarrage de la bourse 9 H
-	{
-			emetteur.emit('rafraichissement'); // lancement de la recuperation des chiffres du cac 40 toute les 5 min 
-			server.heure++;
+if(server.Today.getHours()>8 && server.Today.getHours()<18 && server.Today.getDay()!=6 && server.Today.getDay()!=0)// heure de démarrage de la bourse 9 H
+	{		server.i=0
+			emetteur.emit('recuperation'); // lancement de la recuperation des chiffres du cac 40 toute les 5 min 
+			server.heure++;		
 	}else if(server.Today.getHours()==19 && server.Today.getMinutes()>0 && server.Today.getMinutes()<6 )
 	{
 		server.jour++;
@@ -197,11 +204,27 @@ if(server.Today.getHours()>8 && server.Today.getHours()<18 && server.Today.getDa
 		util.log("c'est pas l'heure");
 	}
 });
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 setInterval(function (){ 
 	emetteur.emit("debut");
 	},30000);
 
+////////////////////////////////////////////GESTION D4ERREURS///////////////////////////////////////////////////////////
+	socket.on('clientError', function(){
+	util.log("erreur connection client");
+	emetteur.emit('recuperation');
+	});
+	socket.on('timeout', function(){
+	util.log("rreur connection client");
+	emetteur.emit('recuperation');
+	});
+	socket.on('drain', function(){
+	util.log("erre connection client");
+	emetteur.emit('recuperation');
+	});
 	
-	
+	emetteur.on('er', function(){
+	util.log("erreur inconue");
+	emetteur.emit('recuperation');
+	});
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
